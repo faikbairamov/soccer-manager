e59ac8dd8b1013f8d649455d4255f16607046e49
Faik Bairamov <faikbairamov0@gmail.com>
2026-04-19 20:14:02 +0400

    fix(db): remote updated_at from users table

--- internal/db/migrations/000001_create_users.up.sql
+++ internal/db/migrations/000001_create_users.up.sql
@@ -2,6 +2,5 @@ CREATE TABLE users (
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email      TEXT NOT NULL UNIQUE,
     password   TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
